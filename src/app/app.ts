3810fd4b057a9f96b2f42fff0c45e7f9027e1483
Andrescg18 <chamolocoporsiempre@gmail.com>
2026-03-05 17:27:41 -0500

    Proyecto venta_motos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { NavbarComponent } from './components/navbar/navbar';
+import { HeroComponent } from './components/hero/hero';
+import { FeaturesComponent } from './components/features/features';
+import { ProductComponent } from './components/product/product';
+import { TestimonialsComponent } from './components/testimonials/testimonials';
+import { CtaComponent } from './components/cta/cta';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    FeaturesComponent,
+    ProductComponent,
+    TestimonialsComponent,
+    CtaComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('carritos');
-}
+export class App {}
